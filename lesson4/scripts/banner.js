68bab6befea0f7d1502d7df252e9b32f81932269
Spencerpaulsen5 <pau21006@byui.edu>
2023-02-23 20:58:26 -0700

    banner

--- lesson4/scripts/banner.js
+++ lesson4/scripts/banner.js
@@ -1,25 +1,5 @@
-let myLandbot = this;
-myLandbot.onLoad(function() { 
-var d = new Date();
-var n = d.getDay();
-var now = d.getHours() + "." + d.getMinutes();
-var weekdays = [
-  ["Sunday"],
-  ["Monday", 8.00, 17.00],
-  ["Tuesday", 8.00, 17.00],
-  ["Wednesday", 8.00, 17.00],
-  ["Thursday", 8.00, 17.00],
-  ["Friday", 8.00, 17.00],
-  ["Saturday"] // we are closed, sorry!
-];
-var day = weekdays[n];
-
-
-if (now > day[1] && now < day[2] || now > day[3] && now < day[4]) {
-  console.log("We're open right now!");
+const d = new Date();
+let day = d.getDay()
+if (day===1||day===2){
+document.getElementById("montue").classList.remove("removebanner");
 }
-else {
-  console.log("Sorry, we're closed!");
- myLandbot.destroy();
-}
-       }) 
