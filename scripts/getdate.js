0fc468150c880692fe53447bdfbbac208e45244e
Spencerpaulsen5 <pau21006@byui.edu>
2024-01-13 22:18:32 -0700

    home page

---
+++ scripts/getdate.js
@@ -0,0 +1,6 @@
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+
+let text = document.lastModified;
+document.getElementById("date").innerHTML = text;
